3de714f4c6968bf318af0aa0d51491c0b17cf03f
luisadnsaavedra <luisadan.saavedradeltoro@gmail.com>
2019-06-18 01:07:32 +0200

    First basic version including two tabs and logos. Empty data

---
+++ iCar/GasViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  iCar
+//
+//  Created by Luis Adán Saavedra del Toro on 17/06/2019.
+//  Copyright © 2019 Luis Adán Saavedra del Toro. All rights reserved.
+//
+
+import UIKit
+
+class GasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    let dataSource = ["5433 km | 2344 litres | 345 $"]//, "45325 km | 34 litres | 234 EUR", "1234 km, 234 litres, 234 £"]
+
+    
+
+}
+
---
+++ iCar/InspectionsServicesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iCar
+//
+//  Created by Luis Adán Saavedra del Toro on 17/06/2019.
+//  Copyright © 2019 Luis Adán Saavedra del Toro. All rights reserved.
+//
+
+import UIKit
+
+class InspectionsServicesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
